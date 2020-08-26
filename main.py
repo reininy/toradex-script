5ed549287161f3863d4740e1dcd83c38f1425c51
Julio Bonon <julio.bonon@toradex.com>
2020-08-26 15:53:50 -0300

    Add linux chromedriver and OOP style

--- main.py
+++ main.py
@@ -1,50 +1,58 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from colors import bcolors
+import json
 import time
+import warnings
 
-
-
-def TorizonNewArticles():
+class bot():
+    warnings.filterwarnings('ignore')
     options = webdriver.ChromeOptions()
-    options.add_argument(r"user-data-dir=D:\Python\Memory\WebWhatsAppBot")
-    driver = webdriver.Chrome("chromedriver.exe", chrome_options=options)
-    url = 'https://developer.toradex.com/knowledge-base?tags=101691&cond=and'
-    driver.get(url)
-    content = driver.find_element_by_class_name('result-tags')
-    html = content.get_attribute("innerHTML")
-    soup= BeautifulSoup(html , 'html.parser')
-    titles = [p.text for p in soup.findAll('li')]
-    driver.close()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
 
-    return titles
+    driver = webdriver.Chrome('./chromedriver', chrome_options=options)
+    file =  open('settings.json', 'r')
+    data = json.load(file)
+    final_data = data['settings']
+
+    def login(self):
+        self.driver.get("https://developer1.toradex.com/acp/default/login")
+        self.driver.find_element_by_name('LoginForm[username]').send_keys(self.final_data['username'])
+        self.driver.find_element_by_name('LoginForm[password]').send_keys(self.final_data['password'])
+        self.driver.find_element_by_name('login-button').click()
+
+    def TorizonNewArticles(self):
+        self.driver.get("https://developer.toradex.com/knowledge-base?tags=101691&cond=and")
+        content = self.driver.find_element_by_class_name('result-tags')
+        html = content.get_attribute("innerHTML")
+        soup= BeautifulSoup(html , 'html.parser')
+        titles = [p.text for p in soup.findAll('li')]
+        
+        return titles
 
 
+    def DeveloperBot(self, titles):
+        self.login()
+        self.driver.get("https://developer1.toradex.com/acp/content/post/update?id=103348")
+        content = self.driver.find_element_by_xpath('//*[@id="post-content_text"]')
+        html = content.get_attribute("innerHTML")
+        
+        for title in titles:
+            if title in html:
+                print(title + bcolors.OKGREEN + ": OK" + bcolors.ENDC)
+            else:
+                print(title + bcolors.WARNING + ": No" + bcolors.ENDC)
+            
 
-def DeveloperBot(titles):
-    options = webdriver.ChromeOptions()
-    options.add_argument(r"user-data-dir=D:\Python\Memory\WebWhatsAppBot")
-    driver = webdriver.Chrome("chromedriver.exe", chrome_options=options)
-    driver.get("https://developer1.toradex.com/acp/content/post/update?id=103348")
-
-    content = driver.find_element_by_xpath('//*[@id="post-content_text"]')
-    html = content.get_attribute("innerHTML")
-    
-    for title in titles:
-        if title in html:
-            print(title + ": OK")
-        else:
-            print(title + ": No")
-            content.send_keys(title + "\n")
-
-    driver.find_element_by_class_name("fa-save").click()
-
-    time.sleep(10)
+        time.sleep(10)
+        self.driver.close()
     
 
 if __name__ == "__main__":
-    titles = TorizonNewArticles()
-    #print(titles)
-    DeveloperBot(titles)
+    bot = bot()
+    titles = bot.TorizonNewArticles()
+    bot.DeveloperBot(titles)
 
 
 
